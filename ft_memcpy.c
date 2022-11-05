0939ac2e9c6323f8c5dbae0cb239fd8712644cdc
Talal Miftah <tmiftah@e2r1p4.1337.ma>
2022-11-05 13:10:46 +0100

    done

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -21,8 +21,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	i = 0;
 	c1 = (unsigned char *)dest;
 	c2 = (unsigned char *)src;
-	if (dest == src)
-		return (dest);
 	if (!c1 && !c2)
 		return (0);
 	while (i < n)
